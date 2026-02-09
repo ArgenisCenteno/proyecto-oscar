89c75282741579893cf2cdb3965f8e233736940e
Argenis J. Centeno <argeniscentenno@gmail.com>
2026-02-08 22:09:19 -0400

    ajuste

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,9 +36,9 @@ class AppServiceProvider extends ServiceProvider
         View::composer('*', function ($view) {
             if (auth()->check()) {
                 $tasa = BcvRate::latest()->first();
-                $dollar = $tasa ? $tasa->precio : 344.51;
+                $dollar = $tasa ? $tasa->precio :  382.63;
             } else {
-                 $dollar = 344.51;
+                 $dollar = 382.63;
                  
             }
             
