7ab651daefede053901951e20c9667cc6284ae86
Argenis J. Centeno <argeniscentenno@gmail.com>
2026-02-08 20:12:50 -0400

    fjafjajfa

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BcvRate;
 use App\Models\Categoria;
+use App\Models\Promocion;
 use Illuminate\Http\Request;
 use App\Models\Direccion;
 use App\Models\CartItem;
